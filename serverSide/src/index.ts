2eacae6609051a92a8137f979ea7b08f6cacb94f
Michael_Veliz <corozomichel@gmail.com>
2026-03-27 15:05:31 -0700

    Started MySQL Server and created tables

---
+++ serverSide/src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { pool } from "./database/pool.js";
+import todoRouter from "./routes/toDoRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Global Middlewares 
+app.use(cors());
+app.use(express.json());
+
+// Rutes
+console.log("Routes loaded");
+app.use("/api/todos", todoRouter);
+// app.use("/api/auth", authRoutes);
+
+// Health check
+app.get("/", (_req, res) => {
+  res.send("🚀 API is running");
+});
+
+const startServer = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("✅ MySQL connected");
+    connection.release();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Unable to connect to MySQL:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
